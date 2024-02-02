b02be1bd3828edda6f741fe6d9aecce3ba911ae0
Jacob <46667568+jacobvd0@users.noreply.github.com>
2024-02-02 00:11:28 +0000

    error handling

--- input average.cpp
+++ input average.cpp
@@ -6,20 +6,31 @@ int main() {
     
     while (true) {
         float currentNum;
-        std::cout << "Add a new number or press q to quit:\n";
+        std::cout << "Add a new number or enter q to quit:\n";
         std::cin >> currentNum;
         std::cout << "\n";
         if (std::cin.fail()) {
             std::cin.clear();
-            std::cin.ignore(std::cin.rdbuf()->in_avail());
-            break;
+            char inputChar = '.';
+            std::cin >> inputChar;
+            if (inputChar == 'Q' || inputChar == 'q') {
+                std::cin.ignore(std::cin.rdbuf()->in_avail());
+                break;
+            } else {
+                std::cin.ignore(std::cin.rdbuf()->in_avail());
+                std::cout << "Invalid input."
+            }
         } else {
             combinedNumbers = combinedNumbers + currentNum;
             totalNumbers++;
         }
     }
+    if (totalNumbers == 0) {
+        std::cout << "Unable to average numbers as no numbers were entered\n";
+    } else {
+        std::cout << "You entered " << totalNumbers << " numbers that averaged " << (combinedNumbers / totalNumbers) << "\n";
+    }
     
-    std::cout << "You entered " << totalNumbers << " numbers that averaged " << (combinedNumbers / totalNumbers) << "\n";
     
     return 0;
 }
